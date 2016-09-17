fbbcc46838d3ba7fbab801b0cf621db19b10c7e7
michaelpd09 <michaelpd09@gmail.com>
2016-09-17 17:50:09 -0400

    Mostrar Nombre

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.mostrar = new System.Windows.Forms.Button();
+            this.mostrarNombre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(136, 46);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(220, 20);
+            this.nombre.TabIndex = 1;
+            // 
+            // mostrar
+            // 
+            this.mostrar.Location = new System.Drawing.Point(333, 98);
+            this.mostrar.Name = "mostrar";
+            this.mostrar.Size = new System.Drawing.Size(75, 23);
+            this.mostrar.TabIndex = 2;
+            this.mostrar.Text = "Mostrar";
+            this.mostrar.UseVisualStyleBackColor = true;
+            this.mostrar.Click += new System.EventHandler(this.mostrar_Click);
+            // 
+            // mostrarNombre
+            // 
+            this.mostrarNombre.AutoSize = true;
+            this.mostrarNombre.Location = new System.Drawing.Point(152, 177);
+            this.mostrarNombre.Name = "mostrarNombre";
+            this.mostrarNombre.Size = new System.Drawing.Size(0, 13);
+            this.mostrarNombre.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 290);
+            this.Controls.Add(this.mostrarNombre);
+            this.Controls.Add(this.mostrar);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.Button mostrar;
+        private System.Windows.Forms.Label mostrarNombre;
+    }
+}
+
